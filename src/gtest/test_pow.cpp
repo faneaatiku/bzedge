23606a1eadabca03c248b6792ac1e6099c5aac7c
nick <33939323+NickRHill@users.noreply.github.com>
2018-07-24 01:41:54 +0100

    update test

--- src/gtest/test_pow.cpp
+++ src/gtest/test_pow.cpp
@@ -24,7 +24,7 @@ TEST(PoW, DifficultyAveraging) {
     // Result should be the same as if last difficulty was used
     arith_uint256 bnAvg;
     bnAvg.SetCompact(blocks[lastBlk].nBits);
-    EXPECT_EQ(CalculateNextWorkRequired(bnAvg,
+    EXPECT_EQ(ZC_CalculateNextWorkRequired(bnAvg,
                                         blocks[lastBlk].GetMedianTimePast(),
                                         blocks[firstBlk].GetMedianTimePast(),
                                         params),
@@ -41,7 +41,7 @@ TEST(PoW, DifficultyAveraging) {
 
     // Result should be the same as if last difficulty was used
     bnAvg.SetCompact(blocks[lastBlk].nBits);
-    EXPECT_EQ(CalculateNextWorkRequired(bnAvg,
+    EXPECT_EQ(ZC_CalculateNextWorkRequired(bnAvg,
                                         blocks[lastBlk].GetMedianTimePast(),
                                         blocks[firstBlk].GetMedianTimePast(),
                                         params),
@@ -54,7 +54,7 @@ TEST(PoW, DifficultyAveraging) {
 
     // Result should not be the same as if last difficulty was used
     bnAvg.SetCompact(blocks[lastBlk].nBits);
-    EXPECT_NE(CalculateNextWorkRequired(bnAvg,
+    EXPECT_NE(ZC_CalculateNextWorkRequired(bnAvg,
                                         blocks[lastBlk].GetMedianTimePast(),
                                         blocks[firstBlk].GetMedianTimePast(),
                                         params),
