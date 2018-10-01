1ff93d3f8e4dca4a7d82d376fd0e2b1c2633b4b6
nick <33939323+NickRHill@users.noreply.github.com>
2018-10-01 15:43:25 +0100

    16 wk deprecation

--- src/deprecation.h
+++ src/deprecation.h
@@ -8,12 +8,12 @@
 // Deprecation policy:
 // * Shut down 16 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 200100;
-static const int WEEKS_UNTIL_DEPRECATION = 8;
+static const int APPROX_RELEASE_HEIGHT = 246000;
+static const int WEEKS_UNTIL_DEPRECATION = 16;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 60); //update to 7*24*60 around fork time
 
 // Number of blocks before deprecation to warn users
-static const int DEPRECATION_WARN_LIMIT = 14 * 24 * 24; // 2 weeks
+static const int DEPRECATION_WARN_LIMIT = 14 * 24 * 60; // 2 weeks
 
 /**
  * Checks whether the node is deprecated based on the current block height, and
